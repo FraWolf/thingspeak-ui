c2e0906357f9ce6b6a15c01343fead9df2034320
Fra <frawolf@outlook.it>
2021-02-24 18:22:31 +0100

    Added the APK of the application

---
+++ src/components/Modals/DownloadModal.js
@@ -0,0 +1,47 @@
+function DownloadModal() {
+  return (
+    <div
+      className="modal fade"
+      id="downloadAppModal"
+      tabIndex="-1"
+      aria-labelledby="downloadAppModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="downloadAppModalLabel">
+              Download
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div>
+              Ecco a te il QR Code, da scansionare con il tuo dispostivo, per
+              scaricare l'applicazione!
+            </div>
+            <div className="text-center">
+              <img src="/img/qrcode.png" height="50%" />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Chiudi
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DownloadModal;
